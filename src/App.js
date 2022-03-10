5ca036fbcc2589f85db442a35457994260206294
WeerayoothM <weerayooth.ma@gmail.com>
2022-03-10 20:54:13 +0700

    add test case for search input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import './App.css';
-import './tailwind.output.css';
+import React, { useState } from "react";
+import { getHeroDetail } from "./api";
+import "./tailwind.output.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    const response = await getHeroDetail(text);
+    setData(response);
+    setLoading(false);
+  };
   return (
-    <div>
+    <div className="max-w-xl mx-auto">
+      <label
+        className="block text-gray-700 text-sm font-bold mb-2"
+        htmlFor="hero-name"
+      >
+        Search
+      </label>
+
+      <div>
+        <input
+          className="shadow appearance-none border rounded py-2 px-2"
+          id="hero-name"
+          placeholder="Type hero name"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button
+          className="inline-flex rounded bg-blue-600 hover:bg-blue-500 font-bold text-white py-2 px-2"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </div>
+      {loading && <div>loading</div>}
+      {data && (
+        <div>
+          <img src={data.avatar} alt={`Avatar of ${data.name}`} />
+          <div>
+            <div>{data.name}</div>
+            <div>{data.description}</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,85 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import {
+  act,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
+import { getHeroDetail } from "./api";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("./api");
+
+const SUPERMAN = {
+  id: 1,
+  name: "Superman",
+  avatar:
+    "https://cdn.theatlantic.com/thumbor/xuePShEYRyEQec_THgWcYFhYLnw=/540x0:2340x1800/500x500/media/img/mt/2016/01/superman/original.jpg",
+  description:
+    "Superman is a fictional superhero. The character was created by writer Jerry Siegel and artist Joe Shuster, and first appeared in the comic book Action Comics #1 (cover-dated June 1938 and published April 18, 1938).[1] The character regularly appears in comic books published by DC Comics, and has been adapted to a number of radio serials, movies, and television shows.",
+};
+
+const renderApp = () => {
+  render(<App />);
+  const getInput = () => screen.getByLabelText(/search/i);
+  const getSubmitButton = () => screen.getByRole("button", { name: /submit/i });
+
+  return {
+    getInput,
+    getSubmitButton,
+    runSearchJourney: async (name, pendingCallback) => {
+      userEvent.type(getInput(), name);
+      userEvent.click(getSubmitButton()); //input
+      if (pendingCallback) pendingCallback();
+      await waitForElementToBeRemoved(() => screen.getByText("loading"));
+    },
+  };
+};
+
+describe("App", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  it("should display input and submit button", () => {
+    const { getInput, getSubmitButton } = renderApp();
+    // test input existence
+    getInput(); //input
+    getSubmitButton();
+    // get vs query
+    // query ใช้เมื่อต้องการเทสว่า component นั้นไม่ได้อยู่ในหน้าจอ
+  });
+
+  it("should call get hero detail with name when submited", async () => {
+    getHeroDetail.mockResolvedValue(SUPERMAN);
+    const { getInput, getSubmitButton } = renderApp();
+
+    userEvent.type(getInput(), "superman");
+    userEvent.click(getSubmitButton()); //input
+
+    await waitFor(() => {
+      expect(getHeroDetail).toHaveBeenCalledWith("superman");
+    });
+  });
+
+  it("should render loading while calling api", async () => {
+    const { runSearchJourney } = renderApp();
+
+    await runSearchJourney("superman", () => {
+      expect(screen.getByText(/loading/i)).toBeInTheDocument();
+    });
+
+    expect(getHeroDetail).toHaveBeenCalledWith("superman");
+  });
+
+  it("should render avatar name & description from api", async () => {
+    const { runSearchJourney } = renderApp();
+
+    await runSearchJourney("superman");
+
+    screen.getByText(SUPERMAN.name);
+    screen.getByText(SUPERMAN.description);
+    screen.getByAltText(`Avatar of ${SUPERMAN.name}`);
+  });
 });
